c191cac56d37c10b15db5303239e5212d446654b
jeff0518 <cycivs07@icloud.com>
2024-01-26 10:13:53 +0800

    feat:完成目前頁面的所有翻譯

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
+
 import Root from "./routes/root";
-// import Header from "./components/layout/Header";
-// import Meals from "./components/meals/Meals";
-// import Cart from "./components/cart/Cart";
-// import SendOut from "./components/cart/SendOut";
-// import { CartContextProvider } from "./context/CartContext";
-// import { UserProgressContextProvider } from "./context/UserProgressContext.";
 import "../src/styles/globals.scss";
 
 function App() {
@@ -13,14 +8,6 @@ function App() {
     <BrowserRouter>
       <Root />
     </BrowserRouter>
-    // <UserProgressContextProvider>
-    //   <CartContextProvider>
-    //     <Header />
-    //     <Meals />
-    //     <Cart />
-    //     <SendOut />
-    //   </CartContextProvider>
-    // </UserProgressContextProvider>
   );
 }
 
--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -2,9 +2,11 @@ import { useNavigate } from "react-router-dom";
 
 import ButtonUI from "../UI/ButtonUI";
 import style from "./Error.module.scss";
+import { useTranslation } from "react-i18next";
 
 function Error() {
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   function goToMainPageHandler() {
     navigate("/main");
@@ -14,9 +16,9 @@ function Error() {
     <div className={style.container}>
       <div>
         <h1>Oops!</h1>
-        <p>抱歉！出現錯誤，請按下方按鈕回到主頁面</p>
+        <p>{t("error")}</p>
         <ButtonUI btnStyle="btn__link" onClick={goToMainPageHandler}>
-          回到主頁面
+          {t("return")}
         </ButtonUI>
       </div>
     </div>
